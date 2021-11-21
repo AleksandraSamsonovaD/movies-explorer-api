1ed141aa106adde59299ee8babd51d548631bd0b
Aleksandra <samsonova183@gmail.com>
2021-11-21 07:17:58 +0300

    cors, port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -8,8 +9,17 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { limiter } = require('./middlewares/limiter');
 const { errorHandler } = require('./errors/error-handler');
 
-const { PORT = 3000, BDNAME = 'bitfilmsdb' } = process.env;
+const { PORT = 3001, BDNAME = 'bitfilmsdb' } = process.env;
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'POST,GET,PATCH,DELETE',
+  allowedHeaders: 'origin, content-type, accept',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
